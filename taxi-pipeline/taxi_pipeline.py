8b86c7d619bdef77258d2a34188138e363833a17
Matthew Copple <mcopple@kcopensource.org>
2026-02-28 19:42:25 -0600

    taxi-pipeline

---
+++ taxi-pipeline/taxi_pipeline.py
@@ -0,0 +1,39 @@
+"""NYC Taxi data pipeline using dlt REST API source."""
+
+import dlt
+from dlt.sources.rest_api import rest_api_source
+
+
+source = rest_api_source({
+    "client": {
+        "base_url": "https://us-central1-dlthub-analytics.cloudfunctions.net/data_engineering_zoomcamp_api",
+    },
+    "resources": [
+        {
+            "name": "trips",
+            "endpoint": {
+                "path": "/",
+                "paginator": {
+                    "type": "page_number",
+                    "base_page": 1,
+                    "page_param": "page",
+                    "total_path": None,
+                    "stop_after_empty_page": True,
+                },
+            },
+        },
+    ],
+})
+
+
+if __name__ == "__main__":
+    pipeline = dlt.pipeline(
+        pipeline_name="taxi_pipeline",
+        destination="duckdb",
+        dataset_name="taxi_data",
+        progress="log",
+        dev_mode=True,
+    )
+
+    load_info = pipeline.run(source)
+    print(load_info)
